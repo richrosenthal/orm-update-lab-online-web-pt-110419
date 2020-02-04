5ddfb161b1ea9a2e4b87646b3b2282934967a1ca
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-02-04 17:38:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,19 @@ def initialize(id = nil, name, grade)
   @grade = grade 
 end 
 
+ def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    )
+    SQL
 
+    DB[:conn].execute(sql)
+  end
+  
+  
 
 
   # Remember, you can access your database connection anywhere in this class
