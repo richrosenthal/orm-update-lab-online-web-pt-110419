01ca0e1c94a5a52700194c4fe35a4e18ea420576
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-02-04 17:47:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -79,7 +79,7 @@ end
 end
 
   def update
-    sql = "UPDATE students SET name = ?, grade = ? WHERE name = ?"
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
   
